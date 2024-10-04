8e37b573bee566ad8eb000e94f369f7132ae074e
AliciaMA <a24aliciaMA@iessanclemente.net>
2024-10-04 14:25:12 +0200

    0410

---
+++ UD1/UD03_1_FragmentsEncrypt/app/src/main/java/com/example/ud03_1_fragmentsencrypt/WelcomeFragment.kt
@@ -0,0 +1,19 @@
+package com.example.ud03_1_fragmentsencrypt
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //no ce toca, infla el diseño desde el fragmento
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
